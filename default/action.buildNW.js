1b1e990b635a811ccd59748a0c5a5a76249399fb
akdombrowski <akdflyinvee73@gmail.com>
2020-03-02 20:13:40 -0600

    NW start

---
+++ default/action.buildNW.js
@@ -0,0 +1,136 @@
+const smartMove = require("./action.smartMove");
+
+function build(creep, flag, room) {
+  let targetId = "5bbcaeeb9099fc012e639c4a";
+  let target = Game.getObjectById(targetId);
+  let building = true;
+  let retval = -16;
+  let rm = creep.room;
+  let rmName = rm.name;
+  let name = creep.name;
+
+  if (rmName !== room) {
+    return smartMove(creep, flag, 1);
+  }
+
+  if (
+    !creep.store[RESOURCE_ENERGY] ||
+    creep.store.getUsedCapacity(RESOURCE_ENERGY) <= 0 ||
+    creep.memory.getEnergy
+  ) {
+    creep.memory.getEnergy = true;
+    target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+
+    if (creep.pos.isNearTo(target)) {
+      retval = creep.harvest(target);
+    } else if (creep.fatigue > 0) {
+      retval = ERR_TIRED;
+      creep.say("f." + creep.fatigue);
+    } else {
+      retval = smartMove(creep, target, 1);
+    }
+
+    if (creep.store.getFreeCapacity(RESOURCE_ENERGY) <= 0) {
+      creep.memory.getEnergy = false;
+    }
+  } else {
+    creep.memory.build = true;
+    creep.memory.getEnergy = false;
+
+    target = Game.getObjectById(targetId);
+
+    if (
+      !target ||
+      !CONSTRUCTION_COST[target.structureType] ||
+      target.progress >= target.progressTotal
+    ) {
+      let t;
+      if (!Memory.e36n32sites || Memory.e36n32sites) {
+        Memory.e36n32sites = Game.rooms.E34N32.find(FIND_CONSTRUCTION_SITES, {
+          filter: site => {
+            let prog = site.progress;
+            let progTot = site.progressTotal;
+            let progLeft = progTot - prog;
+            let type = site.structureType;
+            if (prog >= progTot) {
+              return false;
+            } else if (type === STRUCTURE_EXTENSION) {
+              extFound = true;
+              t = site;
+            } else {
+              return site;
+            }
+          },
+        });
+      }
+
+      let arr = [];
+      _.forEach(Memory.e36n32sites, site => {
+        return arr.push(Game.getObjectById(site.id));
+      });
+
+      target = creep.pos.findClosestByRange(arr);
+      targetId = target ? target.id : null;
+    }
+
+    if (creep.pos.inRangeTo(target, 3)) {
+      if (
+        creep.pos.findInRange(FIND_CREEPS, 1).pop().name !== creep.name &&
+        creep.pos.isNearTo(Game.getObjectById(Memory.source1eRm))
+      ) {
+        retval = creep.move(LEFT);
+
+        if (retval !== OK) {
+          retval = creep.move(TOP_LEFT);
+        }
+
+        if (retval !== OK) {
+          retval = creep.move(RIGHT);
+        }
+
+        if (retval !== OK) {
+          retval = creep.move(TOP_RIGHT);
+        }
+
+        if (retval !== OK) {
+          retval = creep.move(BOTTOM);
+        }
+
+        if (retval !== OK) {
+          retval = creep.move(BOTTOM_RIGHT);
+        }
+
+        creep.say("pass");
+      } else {
+        retval = creep.build(target);
+        creep.memory.b = targetId;
+        creep.say("build");
+      }
+    } else {
+      if (creep.fatigue > 0) {
+        creep.say("f" + creep.fatigue);
+        return ERR_TIRED;
+      }
+
+      retval = smartMove(creep, target, 3, "#ffff0f");
+
+      if (retval !== OK) {
+        creep.say("err");
+      } else {
+        creep.say("m");
+      }
+    }
+
+    if (creep.carry.energy <= 0) {
+      creep.memory.building = false;
+      creep.memory.getEnergy = true;
+      retval = ERR_NOT_ENOUGH_ENERGY;
+    } else {
+      retval = -17;
+    }
+  }
+
+  return retval;
+}
+
+module.exports = build;
---
+++ default/action.getEnergyNW.js
@@ -0,0 +1,99 @@
+const transferEnergy = require("./action.transferEnergyEEast");
+const buildRoad = require("./action.buildRoad");
+const transferEnergyE = require("./action.transferEnergyeRm");
+const smartMove = require("./action.smartMove");
+
+function checkForCreepsNearSource(creep, range, sources) {
+  let target = sources[0];
+  let nESource2 = Game.getObjectById("5bbcaf0c9099fc012e63a0bb");
+
+  _.forEach(sources, src => {
+    let sourceCreeps = target.pos.findInRange(FIND_CREEPS, range);
+    if (sourceCreeps.length > 2 && !_.any(sourceCreeps, creep)) {
+      target = nESource2;
+    }
+  });
+
+  return target;
+}
+
+function vest(creep, flag, path) {
+  creep.memory.direction = "ne";
+  const eastSource1 = Game.getObjectById("5bbcaf0c9099fc012e63a0bd");
+  const nESource1 = Game.getObjectById("5bbcaf0c9099fc012e63a0ba");
+  const nESource2 = Game.getObjectById("5bbcaf0c9099fc012e63a0bb");
+  const name = creep.name;
+  const sourceId = creep.memory.sourceId;
+  let room = creep.room;
+
+
+  let target = sourceId ? Game.getObjectById(sourceId) : null;
+  let retval = -16;
+
+  if (creep.store.getFreeCapacity(RESOURCE_ENERGY) <= 0) {
+    creep.memory.getEnergy = false;
+
+    if (creep.memory.buildingRoad) {
+      let retval = buildRoad(creep);
+      if (retval != OK) {
+        creep.memory.transfer = true;
+        transferEnergyE(creep);
+      } else {
+        creep.memory.buildingRoad = true;
+      }
+    } else if (creep.memory.transfer) {
+      transferEnergyE(creep);
+    }
+    return retval;
+  }
+
+  if(room.name !== "E36N32") {
+    retval = smartMove(creep, Game.flags.e36n32contr, 10);
+    return retval;
+  }
+
+  creep.memory.transfer = false;
+  creep.memory.getEnergy = true;
+
+  if (!target) {
+    target = nESource1;
+    target = checkForCreepsNearSource(creep, 1, [nESource1, nESource2]);
+  }
+
+  if (target) {
+    if (creep.pos.isNearTo(target)) {
+      retval = creep.harvest(target);
+      creep.say("h");
+      creep.memory.sourceId = target.id;
+
+      return retval;
+    } else if (creep.fatigue > 0) {
+      creep.say("f." + creep.fatigue);
+
+      return retval;
+    } else {
+      retval = smartMove(creep, target, 1, true, "#000fff", 2000, 1000);
+      return retval;
+    }
+  } else if (!target) {
+    target = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
+    if (target) {
+      if (creep.pos.isNearTo(target.pos)) {
+        creep.say("pu");
+        retval = creep.pickup(target);
+      } else {
+        creep.say("pu");
+        retval = smartMove(creep, target, 1);
+      }
+
+      creep.memory.sourceId = null;
+      return retval;
+    }
+  } else {
+    console.log("creep.name " + creep.name + " is sad.");
+    creep.say("sad." + retval);
+  }
+  return retval;
+}
+
+module.exports = vest;

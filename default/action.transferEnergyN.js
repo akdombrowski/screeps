ddcd50691950c272e3f0a624f6d3f2bffce6b192
akdombrowski <akdflyinvee73@gmail.com>
2020-03-23 07:40:18 -0500

    wait to transfertower

--- default/action.transferEnergyN.js
+++ default/action.transferEnergyN.js
@@ -21,12 +21,12 @@ function tranN(creep, flag, dest) {
       target &&
       target.structureType === STRUCTURE_TOWER &&
       rm &&
-      enAvail < 600
+      enAvail < 800
     ) {
       target = null;
     }
 
-    if (enAvail > 600 && !target) {
+    if (enAvail > 800 && !target) {
       target = towers[0];
       target = _.find(towers, tower => {
         // tower doesn't exist or doesn't have an energy component

34cafcf82439883ec80aed3f939da40f0d46acb9
akdombrowski <akdflyinvee73@gmail.com>
2021-12-20 16:36:33 -0600

    filter exts list before checking if it needs refreshing

--- default/memoryE59S47ExtensionsRefresh.js
+++ default/memoryE59S47ExtensionsRefresh.js
@@ -2,18 +2,19 @@ const profiler = require("./screeps-profiler");
 const { findMyStructs } = require("./findMyStructs");
 
 function memoryE59S47ExtensionsRefresh() {
-  if (!Memory.e59s47extensions || Memory.e59s47extensions.length <= 0) {
-    Memory.e59s47extensions = findMyStructs(
-      [STRUCTURE_EXTENSION],
-      Memory.northRoomName
-    );
-  } else {
+  if (Memory.e59s47extensions && Memory.e59s47extensions.length > 0) {
     Memory.e59s47extensions = Memory.e59s47extensions.filter(
       (struct) =>
         struct.store && struct.store.getFreeCapacity(RESOURCE_ENERGY) > 0
     );
   }
 
+  if (!Memory.e59s47extensions || Memory.e59s47extensions.length <= 0) {
+    Memory.e59s47extensions = findMyStructs(
+      [STRUCTURE_EXTENSION],
+      Memory.northRoomName
+    );
+  }
   return Memory.e59s47extensions;
 }
 exports.memoryE59S47ExtensionsRefresh = memoryE59S47ExtensionsRefresh;

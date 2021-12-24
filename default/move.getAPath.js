1327cd74b8240177263c561c6430b6814fc1d8f2
akdombrowski <akdflyinvee73@gmail.com>
2021-12-23 18:33:18 -0600

    cleanup getPath

--- default/move.getAPath.js
+++ default/move.getAPath.js
@@ -1,5 +1,58 @@
 const moveAwayFromCreep = require("./action.moveAwayFromCreep");
 
+function findAPath(
+  destPos,
+  opts,
+  path,
+  serPath,
+  desPath,
+  e,
+  name,
+  isOnPath,
+  checkLastStop,
+  range
+) {
+  PathFinder.use(true);
+  let ret = creep.room.findPath(creep.pos, destPos, opts);
+  path = ret;
+
+  try {
+    serPath = path;
+    desPath = Room.deserializePath(serPath);
+    creep.memory.path = JSON.stringify(serPath);
+  } catch (e) {
+    console.log(name + " no despath");
+    return null;
+  }
+
+  if (desPath) {
+    lastStop = desPath[desPath.length - 1];
+    isOnPath = _.find(desPath, (step) => {
+      return creep.pos.isNearTo(step.x, step.y);
+    });
+
+    checkLastStop = false;
+
+    if (destPos && creep.room !== destPos.roomName) {
+      checkLastStop = true;
+    }
+
+    if (lastStop && destPos.inRangeTo(lastStop.x, lastStop.y, range)) {
+      checkLastStop = true;
+    }
+
+    if (!isOnPath || !checkLastStop) {
+      path = null;
+    }
+  }
+
+  if (serPath && checkLastStop && isOnPath) {
+    creep.memory.path = serPath;
+    return serPath;
+  }
+
+  return null;
+}
 function getAPath(
   creep,
   dest,
@@ -32,34 +85,6 @@ function getAPath(
     return null;
   }
 
-  // keep them separate to allow fine tuning of maxOps
-  if (name === "claimNE") {
-    maxOps = 1000;
-  } else if (roll === "upCNE") {
-    maxOps = 1000;
-  } else if (roll === "upCN") {
-    maxOps = 500;
-  } else if (name.endsWith("W")) {
-    maxOps = 500;
-  } else if (name.startsWith("claim")) {
-    maxOps = 200;
-  } else if (name.endsWith("E")) {
-    maxOps = 500;
-  } else if (roll === "neBuilder") {
-    maxOps = 500;
-  } else if (roll === "worker" && direction === "eeast") {
-    maxOps = 500;
-  } else if (name.startsWith("etowerHarvester")) {
-    maxOps = 500;
-  } else if (name.startsWith("eeUp")) {
-    maxOps = 200;
-  } else if (
-    creep.memory.sourceDir === "eeast" &&
-    (roll === "worker" || roll === "eeworker")
-  ) {
-    maxOps = 500;
-  }
-
   let destPos = dest;
   if (destPos && (dest.room || dest.roomName)) {
     let rmName = dest.roomName;
@@ -73,8 +98,6 @@ function getAPath(
   } else {
     return null;
   }
-
-
 
   let opts = creep.memory.opts;
   let maxCost = 10000;
@@ -90,7 +113,7 @@ function getAPath(
       ignoreCreeps: false,
       maxRooms: 4,
 
-      costCallback: function(roomName, costMatrix) {
+      costCallback: function (roomName, costMatrix) {
         let room = Game.rooms[roomName];
         // In this example `room` will always exist, but since
         // PathFinder supports searches which span multiple rooms
@@ -101,7 +124,7 @@ function getAPath(
         let creepCost = 255;
         let ter = new Room.Terrain(roomName);
 
-        room.find(FIND_STRUCTURES).forEach(function(struct) {
+        room.find(FIND_STRUCTURES).forEach(function (struct) {
           const x = struct.pos.x;
 
           const y = struct.pos.y;
@@ -139,43 +162,9 @@ function getAPath(
         costMatrix.set(31, 11, 255);
 
         // Avoid creeps in the room
-        room.find(FIND_CREEPS).forEach(function(c) {
+        room.find(FIND_CREEPS).forEach(function (c) {
           costMatrix.set(c.pos.x, c.pos.y, creepCost);
         });
-
-        // if (room.name === "E35N31") {
-        //   let rd = _.find(room.lookForAt(LOOK_STRUCTURES, 29, 14), struct => {
-        //     return struct.structureType === STRUCTURE_ROAD;
-        //   });
-
-        //   if (rd) {
-        //     costMatrix.set(rd.pos.x, rd.pos.y, 0);
-        //   }
-
-        //   rd = _.find(room.lookForAt(LOOK_STRUCTURES, 30, 14), struct => {
-        //     return struct.structureType === STRUCTURE_ROAD;
-        //   });
-
-        //   if (rd) {
-        //     costMatrix.set(rd.pos.x, rd.pos.y, 0);
-        //   }
-
-        //   rd = _.find(room.lookForAt(LOOK_STRUCTURES, 30, 15), struct => {
-        //     return struct.structureType === STRUCTURE_ROAD;
-        //   });
-
-        //   if (rd) {
-        //     costMatrix.set(rd.pos.x, rd.pos.y, 0);
-        //   }
-
-        //   rd = _.find(room.lookForAt(LOOK_STRUCTURES, 30, 16), struct => {
-        //     return struct.structureType === STRUCTURE_ROAD;
-        //   });
-
-        //   if (rd) {
-        //     costMatrix.set(rd.pos.x, rd.pos.y, 0);
-        //   }
-        // }
 
         Memory.costMatrix = JSON.stringify(costMatrix.serialize());
         return costMatrix;
@@ -184,104 +173,21 @@ function getAPath(
     creep.memory.costMatrix = opts;
   }
 
-  PathFinder.use(true);
-  let ret = creep.room.findPath(creep.pos, destPos, opts);
-  path = ret;
+  let path = findAPath(
+    destPos,
+    opts,
+    path,
+    serPath,
+    desPath,
+    e,
+    name,
+    isOnPath,
+    checkLastStop,
+    range
+  );
 
-  try {
-    serPath = path;
-    desPath = Room.deserializePath(serPath);
-    creep.memory.path = JSON.stringify(serPath);
-  } catch (e) {
-    console.log(name + " no despath");
-    return null;
-  }
-
-  if (desPath) {
-    lastStop = desPath[desPath.length - 1];
-    isOnPath = _.find(desPath, step => {
-      return creep.pos.isNearTo(step.x, step.y);
-    });
-
-    checkLastStop = false;
-
-    if (destPos && creep.room !== destPos.roomName) {
-      checkLastStop = true;
-    }
-
-    if (lastStop && destPos.inRangeTo(lastStop.x, lastStop.y, range)) {
-      checkLastStop = true;
-    }
-
-    if (!isOnPath || !checkLastStop) {
-      path = null;
-    }
-  }
-
-  // Check if 1st path try, or path from memory, gets us where we want to go.
-  if (serPath && checkLastStop && isOnPath) {
-    creep.memory.path = serPath;
-    return serPath;
-
-    retval = creep.moveByPath(path, {
-      visualizePathStyle: {
-        fill: "transparent",
-        stroke: "#fff",
-        lineStyle: "dashed",
-        strokeWidth: 0.15,
-        opacity: 0.1,
-      },
-    });
-
-    if (retval === OK) {
-      if (creep.pos.inRangeTo(dest, range)) {
-        creep.memory.path = null;
-      }
-
-      // return retval;
-    } else {
-      path = null;
-      creep.memory.path = null;
-      // second chance path was also out of range
-    }
-  }
-
-  // retval = creep.moveTo(dest, {
-  //   reusePath: pathMem,
-  //   ignoreCreeps: ignoreCreeps,
-  //   range: range,
-  //   maxOps: maxOps,
-  //   serializeMemory: true,
-  //   noPathFinding: noPathFinding,
-  //   visualizePathStyle: { stroke: pathColor },
-  // });
-
-  // if (retval === ERR_INVALID_TARGET || retval === ERR_NOT_FOUND) {
-  //   retval = creep.moveTo(dest, {
-  //     reusePath: pathMem,
-  //     ignoreCreeps: ignoreCreeps,
-  //     range: range,
-  //     maxOps: maxOps,
-  //     serializeMemory: true,
-  //     noPathFinding: false,
-  //     visualizePathStyle: { stroke: "#000000" },
-  //   });
-  // } else if (retval === ERR_NO_PATH) {
-  //   retval = creep.moveTo(dest, {
-  //     reusePath: 0,
-  //     ignoreCreeps: ignoreCreeps,
-  //     range: range,
-  //     maxOps: maxOps * 2,
-  //     serializeMemory: false,
-  //     noPathFinding: false,
-  //     visualizePathStyle: { stroke: "#0FFFFF" },
-  //   });
-  // }
-
-  path = null;
-  creep.memory.path = null;
-  creep.say("null");
-  return null;
+  creep.memory.path = path;
+  return path;
 }
 
 module.exports = getAPath;

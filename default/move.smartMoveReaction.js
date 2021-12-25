dd6f24c2aea4c681a27130ae6b22f5340e7f6a17
akdombrowski <akdflyinvee73@gmail.com>
2021-12-25 08:08:03 -0600

    keep memory of path if succesful move

--- default/move.smartMoveReaction.js
+++ default/move.smartMoveReaction.js
@@ -14,18 +14,22 @@ function smartMoveReaction(
     retval = successfulMove(creep, destPos, path, pathColor, dest, range);
   } else if (retval === ERR_NOT_FOUND) {
     // path doesn't match creep's location
+    path = null;
     creep.say("no match");
   } else if (retval === ERR_INVALID_TARGET) {
+    path = null;
     creep.say("invalidTarget");
   } else if (retval === ERR_NO_PATH) {
+    path = null;
     creep.say("noPath");
   } else {
     // console.log(name + " smartMove oops " + retval);
+    path = null;
     creep.say("oops." + retval);
     // second chance path was also out of range
   }
-  path = null;
-  creep.memory.path = null;
+
+  creep.memory.path = JSON.stringify(path);
   return path;
 }
 exports.smartMoveReaction = smartMoveReaction;

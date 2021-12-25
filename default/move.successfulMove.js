edd2541f2826ac0de72848b8a5d7cbacf5bda28c
akdombrowski <akdflyinvee73@gmail.com>
2021-12-25 08:06:12 -0600

    formatting change opacity to less transparent

--- default/move.successfulMove.js
+++ default/move.successfulMove.js
@@ -2,16 +2,17 @@ const profiler = require("./screeps-profiler");
 
 function successfulMove(creep, destPos, path, pathColor, dest, range) {
   let retval = OK;
-  creep.say(destPos.x + "," + destPos.y);
+
   creep.room.visual.poly(path, {
     stroke: pathColor,
     lineStyle: "dashed",
-    opacity: 0.25,
+    opacity: 0.99,
   });
 
   if (creep.pos.inRangeTo(dest, range)) {
     creep.memory.path = null;
   }
+
   return retval;
 }
 exports.successfulMove = successfulMove;

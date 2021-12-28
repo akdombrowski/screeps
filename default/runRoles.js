78bc8d4983a3ddbcbc14e34f139b99c1cbacab58
akdombrowski <akdflyinvee73@gmail.com>
2021-12-28 15:52:41 -0600

    move to new file

--- default/runRoles.js
+++ default/runRoles.js
@@ -21,76 +21,8 @@ const profiler = require("./screeps-profiler");
 const roleViewer = require("./role.viewer");
 const roleRangedAttacker = require("./role.rangedAttacker");
 const roleRangedAttackerdS = require("./role.rangedAttackerdS");
-const flee = require("./move.flee");
 const roleHarvesterPickerUpper = require("./role.harvester.PickerUpper");
-
-function setCreepRoomArrayAndAvoidInvaders(
-  creep,
-  creepsE59S48,
-  creepsE59S49,
-  creepsE59S47,
-  distanceToInvaderToTriggerFlee
-) {
-  const roll = creep.memory.role;
-  const direction = creep.memory.direction;
-  let retval = -16;
-  let shouldContinueToNextCreep = false;
-
-  if (direction === "south") {
-    creepsE59S48.push(creep.name);
-
-    let invader = Game.getObjectById(Memory.invaderIDE59S48);
-
-    if (invader) {
-      if (
-        roll != "rangedAttacker" &&
-        creep.pos.inRangeTo(invader, distanceToInvaderToTriggerFlee)
-      ) {
-        retval = flee(creep, invader.pos, 20);
-        shouldContinueToNextCreep = true;
-      }
-    }
-  } else if (direction === "deepSouth") {
-    creepsE59S49.push(creep.name);
-
-    let invader = Game.getObjectById(Memory.invaderIDE59S49);
-
-    if (invader) {
-      if (
-        roll != "rangedAttacker" &&
-        creep.pos.inRangeTo(invader, distanceToInvaderToTriggerFlee)
-      ) {
-        retval = flee(creep, invader.pos, 20);
-        shouldContinueToNextCreep = true;
-      }
-    }
-  } else if (direction === "north") {
-    creepsE59S47.push(creep.name);
-
-    let invader = Game.getObjectById(Memory.invaderIDE59S47);
-
-    if (invader) {
-      if (
-        roll != "rangedAttacker" &&
-        creep.pos.inRangeTo(invader, distanceToInvaderToTriggerFlee)
-      ) {
-        retval = flee(creep, invader.pos, 20);
-        shouldContinueToNextCreep = true;
-      }
-    }
-  }
-  return {
-    retval: retval,
-    shouldContinueToNextCreep: shouldContinueToNextCreep,
-    creepsE59S48: creepsE59S48,
-    creepsE59S49: creepsE59S49,
-    creepsE59S47: creepsE59S47,
-  };
-}
-setCreepRoomArrayAndAvoidInvaders = profiler.registerFN(
-  setCreepRoomArrayAndAvoidInvaders,
-  "setCreepRoomArrayAndAvoidInvaders"
-);
+const setCreepRoomArrayAndAvoidInvaders = require("./setCreepRoomArrayAndAvoidInvaders");
 
 function runRoles() {
   let i = 0;
---
+++ default/setCreepRoomArrayAndAvoidInvaders.js
@@ -0,0 +1,65 @@
+const profiler = require("./screeps-profiler");
+const flee = require("./move.flee");
+
+function setCreepRoomArrayAndAvoidInvaders(
+  creep,
+  creepsE59S48,
+  creepsE59S49,
+  creepsE59S47,
+  distanceToInvaderToTriggerFlee
+) {
+  const roll = creep.memory.role;
+  const direction = creep.memory.direction;
+  let retval = -16;
+  let shouldContinueToNextCreep = false;
+
+  if (direction === "south") {
+    creepsE59S48.push(creep.name);
+
+    let invader = Game.getObjectById(Memory.invaderIDE59S48);
+
+    if (invader) {
+      if (roll != "rangedAttacker" &&
+        creep.pos.inRangeTo(invader, distanceToInvaderToTriggerFlee)) {
+        retval = flee(creep, invader.pos, 20);
+        shouldContinueToNextCreep = true;
+      }
+    }
+  } else if (direction === "deepSouth") {
+    creepsE59S49.push(creep.name);
+
+    let invader = Game.getObjectById(Memory.invaderIDE59S49);
+
+    if (invader) {
+      if (roll != "rangedAttacker" &&
+        creep.pos.inRangeTo(invader, distanceToInvaderToTriggerFlee)) {
+        retval = flee(creep, invader.pos, 20);
+        shouldContinueToNextCreep = true;
+      }
+    }
+  } else if (direction === "north") {
+    creepsE59S47.push(creep.name);
+
+    let invader = Game.getObjectById(Memory.invaderIDE59S47);
+
+    if (invader) {
+      if (roll != "rangedAttacker" &&
+        creep.pos.inRangeTo(invader, distanceToInvaderToTriggerFlee)) {
+        retval = flee(creep, invader.pos, 20);
+        shouldContinueToNextCreep = true;
+      }
+    }
+  }
+  return {
+    retval: retval,
+    shouldContinueToNextCreep: shouldContinueToNextCreep,
+    creepsE59S48: creepsE59S48,
+    creepsE59S49: creepsE59S49,
+    creepsE59S47: creepsE59S47,
+  };
+}
+module.exports = setCreepRoomArrayAndAvoidInvaders;
+setCreepRoomArrayAndAvoidInvaders = profiler.registerFN(
+  setCreepRoomArrayAndAvoidInvaders,
+  "setCreepRoomArrayAndAvoidInvaders"
+);

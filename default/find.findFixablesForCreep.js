816901b0ca615f4508b4e8acad835ace0b3dd1b2
akdombrowski <akdflyinvee73@gmail.com>
2022-01-02 19:30:18 -0600

    spelling

--- default/find.findFixablesForCreep.js
+++ default/find.findFixablesForCreep.js
@@ -6,47 +6,47 @@ function findFixablesForCreep(creep, target) {
   const timeToPassForRecheck = 50;
   if (creep.memory.direction.startsWith("s")) {
     if (
-      !Memory.homefixables ||
-      Memory.homefixables.length <= 0 ||
+      !Memory.homeFixables ||
+      Memory.homeFixables.length <= 0 ||
       Memory.lastHomeCheckFixables - Game.time > timeToPassForRecheck
     ) {
-      Memory.homefixables = findFixables(Game.rooms[Memory.homeRoomName]);
+      Memory.homeFixables = findFixables(Game.rooms[Memory.homeRoomName]);
       Memory.lastHomeCheckFixables = Game.time;
     }
   } else if (creep.memory.direction.startsWith("n")) {
     if (
-      !Memory.northfixables ||
-      Memory.northfixables.length <= 0 ||
+      !Memory.northFixables ||
+      Memory.northFixables.length <= 0 ||
       Memory.lastNorthCheckFixables - Game.time > timeToPassForRecheck
     ) {
-      Memory.northfixables = findFixables(Game.rooms[Memory.northRoomName]);
+      Memory.northFixables = findFixables(Game.rooms[Memory.northRoomName]);
       Memory.lastNorthCheckFixables = Game.time;
     }
   } else if (creep.memory.direction == "south") {
     if (
-      !Memory.southfixables ||
-      Memory.southfixables.length <= 0 ||
+      !Memory.southFixables ||
+      Memory.southFixables.length <= 0 ||
       Memory.lastSouthCheckFixables - Game.time > timeToPassForRecheck
     ) {
-      Memory.southfixables = findFixables(Game.rooms[Memory.southRoomName]);
+      Memory.southFixables = findFixables(Game.rooms[Memory.southRoomName]);
       Memory.lastSouthCheckFixables = Game.time;
     }
   } else if (creep.memory.direction == "west") {
     if (
-      !Memory.westfixables ||
-      Memory.westfixables.length <= 0 ||
+      !Memory.westFixables ||
+      Memory.westFixables.length <= 0 ||
       Memory.lastWestCheckFixables - Game.time > timeToPassForRecheck
     ) {
-      Memory.westfixables = findFixables(Game.rooms[Memory.westRoomName]);
+      Memory.westFixables = findFixables(Game.rooms[Memory.westRoomName]);
       Memory.lastWestCheckFixables = Game.time;
     }
   } else {
     if (
-      !Memory.homefixables ||
-      Memory.homefixables.length <= 0 ||
+      !Memory.homeFixables ||
+      Memory.homeFixables.length <= 0 ||
       Memory.lastHomeCheckFixables - Game.time > timeToPassForRecheck
     ) {
-      Memory.homefixables = findFixables(Game.rooms[Memory.homeRoomName]);
+      Memory.homeFixables = findFixables(Game.rooms[Memory.homeRoomName]);
       Memory.lastHomeCheckFixables = Game.time;
     }
   }

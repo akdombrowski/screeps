8cb522b7f20f5700e85c1ef249f8752685d1a862
akdombrowski <akdflyinvee73@gmail.com>
2022-01-09 07:55:03 -0600

    build targets

--- default/build.js
+++ default/build.js
@@ -70,8 +70,8 @@ function build(creep, targetRoomName, exitDirection, exit) {
     if (!target) {
       if (creepRoomName === Memory.homeRoomName) {
         target = roomBuildTargetPriorities(creep, Memory.homeRoomName, [
-          "61d6a04cbe69f668ba0ee0b1",
           "61d6a04dbe69f67ca00ee0b2",
+          "61d6a0413762ce042d134d7b",
         ]);
       } else if (creepRoomName === Memory.westRoomName) {
         target = roomBuildTargetPriorities(creep, Memory.westRoomName, []);

c4c2bad7d18ffa72fa90c9817209be960a9f7cd2
akdombrowski <akdflyinvee73@gmail.com>
2022-01-16 06:04:14 -0600

    increase maxOps

--- default/move.smartMove.js
+++ default/move.smartMove.js
@@ -47,7 +47,7 @@ function smartMove(
   let desPath;
   pathColor = pathColor || getRandomColor();
   pathMem = pathMem || Math.floor(Math.random() * 100) + 1;
-  maxOps = maxOps || Math.floor(Math.random() * 2000) + 1000;
+  maxOps = maxOps || Math.floor(Math.random() * 1000) + 1500;
   maxRms = maxRms || 1;
   ignoreCreeps = ignoreCreeps || true;
   flee = flee || false;
